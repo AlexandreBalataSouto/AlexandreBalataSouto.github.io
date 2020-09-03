b6d29322ab3a59702fba24cb2b2991dae3baf596
Balata <balatasouto@gmail.com>
2020-09-03 17:45:27 +0100

    Vue-Laravel added

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ var language = {
         kdb: "Here is my Superior Grade Formative Course final project of web application development. The idea was to help a small karate dojo of Lanzarote with the management of the students data, asistence and events. This project was made with <strong>Laravel</strong>, <strong>Materialize</strong> and it´s <strong>responsive.</strong><br><br><strong>Login Access:</strong><br>Email: kdb@example.com Password: csas1234",
         goToKDB: "Go to KDB",
         projectDate04: "Project date: June 2020",
+        vueLaravel: "A SPA (single page application) made with the javascript framework <strong>Vue.js</strong> and the php framework <strong>Laravel</strong>",
+        goToVueLaravel: "Go to Vue-Laravel",
+        projectDate08: "Project date: August 2020",
         
     },
     es: {
@@ -35,6 +38,9 @@ var language = {
         kdb: "Aquí está mi proyecto final del Ciclo Formativo de Grado Superior de desarrollo de aplicaciones web. La idea era ayudar a un pequeño dojo de kárate de Lanzarote con la gestión de los datos, asistencias y eventos de los alumnos. Este proyecto se hizo con <strong>Laravel</strong>, <strong>Materialize</strong> y es <strong>responsive.</strong><br><br><strong>Acceso de inicio de sesión:</strong><br>Email: kdb@example.com Contraseña: csas1234",
         goToKDB: "Ir a KDB",
         projectDate04: "Fecha del proyecto: Junio 2020",
+        vueLaravel: "Una SPA (single page application) hecha con el framework de javascript <strong>Vue.js</strong> y el framework de php <strong>Laravel</strong>",
+        goToVueLaravel: "Ir a Vue-Laravel",
+        projectDate08: "Fecha del proyecto: Agosto 2020",
     }
 }
 
@@ -64,6 +70,9 @@ if (window.location.hash) {
         kdbId.innerHTML = language.es.kdb;
         goToKDB.innerHTML = language.es.goToKDB;
         projectDate04.innerHTML = language.es.projectDate04;
+        vueLaravelId.innerHTML = language.es.vueLaravel;
+        goToVueLaravel.innerHTML = language.es.goToVueLaravel;
+        projectDate08.innerHTML = language.es.projectDate08;
     } else if (window.location.hash === "#eng") {
         welcomeId.innerHTML = language.eng.welcome;
         titleId.innerHTML = language.eng.title;
@@ -82,5 +91,8 @@ if (window.location.hash) {
         kdbId.innerHTML = language.eng.kdb;
         goToKDB.innerHTML = language.eng.goToKDB;
         projectDate04.innerHTML = language.eng.projectDate04;
+        vueLaravelId.innerHTML = language.eng.vueLaravel;
+        goToVueLaravel.innerHTML = language.eng.goToVueLaravel;
+        projectDate08.innerHTML = language.eng.projectDate08;
     }
 }
